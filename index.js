174849a127d5dea05271a8ab0b6ff3e45eda9907
TwinkleRing <dlrmwl15@gmail.com>
2020-11-21 18:45:44 +0900

    bodyparser & postman

--- index.js
+++ index.js
@@ -9,48 +9,41 @@
 const express = require('express') // 다운받은 express 가져옴
 const app = express() // 새로운 express 앱 만들기
 const port = 5000 // 포트는 아무거나 상관없다.
+const bodyParser = require("body-parser");
+const { User }  = require("./models/User");
+// application/x-www-form-urlencoded 가져오기
+app.use(bodyParser.urlencoded({extended: true})); 
+// application/json 가져오기
+app.use(bodyParser.json());
 
 // mongoose 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+
 mongoose.connect('mongodb+srv://hun:dudgns941!@boilerplate.oevwc.mongodb.net/BoilerPlate?retryWrites=true&w=majority', {
     useNewUrlParser : true, useUnifiedTopology: true , useCreateIndex : true, useFindAndModify : false
     // 에러 안뜨게 함
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/',(req,res) => res.send('안녕하세요 ~ !'))
+app.get('/', (req, res) => res.send('안녕하세요 ~ !'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면 
+    // 그것들을 데이터베이스에 넣어준다.
+
+    
+    //bodyparser로 client에서 보내주는 정보를 받는다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success : false ,  err })
+        return res.status(200).json({
+            success : true
+        }) // 성공
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-// 실행 : npm run start
-
-// 몽고 DB 연결하기
-// 1. 몽고 DB 사이트 접속 , 클러스터 만들기, 몽고db 유저 생성
-// id : hun , password : dudgns941!
-
-// mongodb+srv://hun:<password>@boilerplate.oevwc.mongodb.net/<dbname>?retryWrites=true&w=majority
-
-// 몽구스 : 몽고db를 편하게 사용하게 해주는 툴
-
-// user model, user schema 
-// model은 스키마를 감싸주는 역할을 하는 것
-// 스키마는 어떤 상품에 관련된 글을 작성한다면 , 글을 작성하는 사람이 누구인지, 그 포스트의 이름이 무엇인지, 
-// 그리고 타입은 무엇이고 길이는 얼마까지인지 지정해주는 것이 스키마로 하는 일
-
-// #5 . git 설치
-// 분산 버전 관리 시스템 , git --version
-
-// git 저장소 만들기 : git init
-// git status
-// git add .
-// 1. working directory => 2. Staging Area => 3. Git repository (Local) => 4. Git repository (Remote)
-
-// git ignore
-
-// git commit -m " 쓰고 싶은 메세지 "
-
-// #6 . github 연결 (ssh로 github 연결하기)
-
-// ssh 설정하기
-// ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQoflpfSLwGBKojTBfXtcCgoWvoCNxKMBFOfQmFMGjn dlrmwl15@gmail.com
-// https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
