9c45a9e15b127ec15f5889c4a2677e0fd2072812
TwinkleRing <dlrmwl15@gmail.com>
2020-12-02 16:29:31 +0900

    Auth 기능 구현

--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { registerUser } from '../../../_actions/user_action';
-import Axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
 
 function RegisterPage(props) {
@@ -91,5 +91,5 @@ function RegisterPage(props) {
     )
 }
 
-export default RegisterPage
+export default withRouter(RegisterPage)
 
